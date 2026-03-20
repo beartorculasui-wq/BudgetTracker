fe09c6f684a38effed25eef45a1f318d26260ec3
beartorculasui-wq <bear.torculas.ui@phinmaed.com>
2026-03-20 23:15:32 +0800

    Update serviceworker.js

--- serviceworker.js
+++ serviceworker.js
@@ -1,6 +1,6 @@
 self.addEventListener("install", function(e) {
   e.waitUntil(
-    caches.open("Budget Tracker-cache").then(function(cache) {
+    caches.open("BudgetTracker-cache").then(function(cache) {
       return cache.addAll(["index.html"]);
     })
   );
